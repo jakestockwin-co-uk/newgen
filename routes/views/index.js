ff918144b10fe4e8f66c505e68cb005d8e5719d8
jstockwin <jstockwin@gmail.com>
2016-08-21 20:51:52 +0100

    Added blog to homepage

--- routes/views/index.js
+++ routes/views/index.js
@@ -4,6 +4,7 @@ exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
+	locals.posts = [];
 
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
@@ -13,8 +14,25 @@ exports = module.exports = function (req, res) {
 	view.on('init', function (next) {
 		keystone.list('Profile').model.find().exec(function (err, results) {
 			locals.profiles = results;
+		}).then(function () {
+			keystone.list('Post').paginate({
+				page: 1,
+				perPage: 2,
+				maxPages: 1,
+				filters: {
+					state: 'published',
+				},
+			})
+				.sort('-publishedDate')
+				.populate('author categories')
+				.exec(function (err, results) {
+					locals.posts = results;
+					next(err);
+				});
+		}, function (err) {
 			next(err);
 		});
+
 	});
 
 	// Render the view
